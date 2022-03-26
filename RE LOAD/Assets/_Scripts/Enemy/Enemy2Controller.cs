09ccd86ff065afa6c89982ce21aac5796ae23a11
Ziviously <pawarej_u@sci.tu.ac.th>
2022-03-26 17:05:04 +0700

    Enemy 2 Script

---
+++ RE LOAD/Assets/_Scripts/Enemy/Enemy2Controller.cs
@@ -0,0 +1,232 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2Controller : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private LayerMask whatIsPlayer;
+    [SerializeField] private Feet feet;
+    [SerializeField] private GameObject hand;
+    private Vector3 currentDestination;
+    Rigidbody rb;
+    public float moveSpeed;
+
+    [Header("Patrolling")]
+    [Tooltip("only vertical movement patrol")]
+    public bool isStationary;
+    public Vector3[] targetWalkPoints;
+    [SerializeField] private bool TwoDirectionPath; //Dont check this button if you want the enemy to loop 1-2-3-4-3-2-1-2-3-4. Tick this if you want it to go 1-2-3-4-1-2-3-4
+    private bool isReturning;
+    private int walkPointIndex;
+    private Vector3 walkPoint;
+    private bool walkPointSet;
+    public float rememberCountdown;
+    public float rememberTime;
+    public bool remembers;
+    public float confusedTime;
+
+    [Header("Attacking")]
+    [SerializeField] private float timeBetweenAttacks;
+    private bool hasAttacked;
+    public GameObject enemyBullet;
+    [SerializeField] private Transform attackPoint;
+
+    [Header("States")]
+    [SerializeField] private float surroundSightRange;
+    [SerializeField] private float forwardSightRange;
+    [SerializeField] private float sightAngle = 150f;
+    public LayerMask obstructionMask;
+    [Space(10)]
+    [SerializeField] private float attackRange;
+    [SerializeField] private bool playerInSight, playerInAttackRange;
+
+
+    private bool InLosOfPlayer()
+    {
+        if (Physics.CheckSphere(transform.position, forwardSightRange, whatIsPlayer))
+        {
+            Vector3 dir = (player.position - transform.position).normalized;
+            float angle = Vector3.Angle(transform.forward, dir);
+
+            if (angle < sightAngle)
+            {
+                float dist = Vector3.Distance(transform.position, player.position);
+
+                if (!Physics.Raycast(transform.position, dir, dist, obstructionMask))
+                {
+                    rememberCountdown = rememberTime;
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Start()
+    {
+        //player = PlayerController.instance.GetComponent<Transform>();
+        feet = GetComponentInChildren<Feet>();
+        rb = GetComponent<Rigidbody>();
+        //agent = GetComponent<NavMeshAgent>();
+        walkPointIndex = 0;
+
+        if (!isStationary && targetWalkPoints.Length > 0)
+        {
+            //transform.LookAt(targetWalkPoints[walkPointIndex]);
+            isReturning = false;
+            walkPoint = targetWalkPoints[walkPointIndex];
+        }
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSight = Physics.CheckSphere(transform.position, surroundSightRange, whatIsPlayer);
+        if (rememberCountdown > 0)
+        {
+            remembers = true;
+            rememberCountdown -= Time.deltaTime;
+        }
+        else remembers = false;
+
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+
+
+        if (!playerInSight && !playerInAttackRange && !isStationary && !remembers) Patroling();
+        if (playerInSight && !playerInAttackRange) ChasePlayer();
+        if (playerInSight && playerInAttackRange) AttackPlayer();
+
+        if (remembers && !playerInSight && !playerInAttackRange) Confused();
+
+    }
+
+
+    //=================================================================================
+    private void Patroling()
+    {
+        LookAt(walkPoint);
+        SetDestination(walkPoint);
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 0.1f)
+        {
+            SetNextWalkPoint();
+            walkPoint = targetWalkPoints[walkPointIndex];
+        }
+    }
+
+    private void SetNextWalkPoint()
+    {
+        if (!TwoDirectionPath)
+        {
+            if (!isReturning)
+            {
+                if (walkPointIndex < targetWalkPoints.Length - 1) walkPointIndex++;
+                else isReturning = true;
+            }
+            if (isReturning)
+            {
+                if (walkPointIndex > 0) walkPointIndex--;
+                else
+                {
+                    isReturning = false;
+                }
+                if (walkPointIndex < 0) walkPointIndex = 1;
+            }
+        }
+        else
+        {
+            walkPointIndex++;
+            if (walkPointIndex == targetWalkPoints.Length) walkPointIndex = 0;
+        }
+    }
+
+
+    private void Confused()
+    {
+        SetDestination(transform.position);
+        StartCoroutine(Wait());
+    }
+
+    //=================================================================================
+
+    private void ChasePlayer()
+    {
+        SetDestination(player.position);
+        LookAt(player.position);
+
+
+
+        //Debug.Log("chasingPlayer");
+    }
+
+    //=================================================================================
+
+    private void AttackPlayer()
+    {
+        SetDestination(transform.position);
+        //hand.transform.LookAt(player);
+        //attackPoint.transform.LookAt(player);
+        LookAt(player.position);
+        //transform.rotation = Quaternion.Lerp()
+
+        if (!hasAttacked)
+        {
+            //Rigidbody rb = Instantiate(enemyBullet, attackPoint.position, attackPoint.rotation).GetComponent<Rigidbody>();
+            //rb.AddForce(attackPoint.transform.forward * 25f, ForceMode.Impulse);
+
+            hasAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+    }
+
+    private void ResetAttack()
+    {
+        hasAttacked = false;
+    }
+
+    //=================================================================================
+    private void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, surroundSightRange);
+
+        Gizmos.DrawWireSphere(transform.position, forwardSightRange);
+
+        if (InLosOfPlayer()) Gizmos.DrawLine(transform.position, player.position);
+
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
+    private void LookAt(Vector3 pos)
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(pos - transform.position, Vector3.up));
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(confusedTime);
+    }
+
+    void SetDestination(Vector3 pos)
+    {
+        currentDestination = pos;
+    }
+
+    void DestinationExecution()
+    {
+        Vector3 dir = (currentDestination- transform.position).normalized;
+        rb.velocity = dir * moveSpeed;
+    }
+}
