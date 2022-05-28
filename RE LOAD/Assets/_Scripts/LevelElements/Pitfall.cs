892cae35a615002d2a42c2fcfc14aa2105ed1ddb
Thanakit24 <univgworld@gmail.com>
2022-05-28 18:40:42 +0700

    Lvl2Lighting w/ReflectionProbe 90% Done

---
+++ RE LOAD/Assets/_Scripts/LevelElements/Pitfall.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pitfall : MonoBehaviour
+{
+    public Transform newPos;
+
+	[SerializeField] private Image white;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.name == "FinalPlayer")
+		{
+			other.transform.position = newPos.transform.position;
+			StartCoroutine(FadeOut());
+		}
+	}
+
+	IEnumerator FadeOut()
+	{
+		white.gameObject.SetActive(true);
+		for(float f = 1f; f >= -0.05; f-= 0.05f)
+		{
+			Color c = white.GetComponent<Image>().color;
+			c.a = f;
+			white.GetComponent<Image>().color = c;
+			yield return new WaitForSeconds(0.05f);
+		}
+		white.gameObject.SetActive(false);
+	}
+}
