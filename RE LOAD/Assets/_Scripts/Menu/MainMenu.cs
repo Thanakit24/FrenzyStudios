3855f7f5d0c6bea2f01474a1cfba8b3cce017ff1
Ziviously <pawarej_u@sci.tu.ac.th>
2022-05-28 15:48:23 +0700

    Build Set Up

--- RE LOAD/Assets/_Scripts/Menu/MainMenu.cs
+++ RE LOAD/Assets/_Scripts/Menu/MainMenu.cs
@@ -8,13 +8,18 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void Play()
     {
-        SceneManager.LoadScene("Level3Lighting");
+        SceneManager.LoadScene(1);
     }
 
     // Update is called once per frame
     public void LevelSelection()
     {
         SceneManager.LoadScene("LevelSelection");
+    }
+
+    public void Credits()
+    {
+
     }
 
     public void Setting()
