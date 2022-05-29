7f4da3e9fa32b6c55ed4ea551f26b805be837bd9
Ziviously <pawarej_u@sci.tu.ac.th>
2022-05-30 01:32:08 +0700

    asdfghjkl;

--- RE LOAD/Assets/_Scripts/Player/PlayerMelee.cs
+++ RE LOAD/Assets/_Scripts/Player/PlayerMelee.cs
@@ -2,19 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.Feedbacks;
+
 public class PlayerMelee : MonoBehaviour
 {
     public Vector3 meleeKnockback;
     public int damage;
     public MMFeedbacks meleeImpact;
 
+    BoxCollider col;
+
+
+    private void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        Collider[] hits = Physics.OverlapBox(col.center, col.extents/2);
+
+        foreach (var item in hits)
+        {
+            print(item.name);
+            Health hp = item.GetComponent<Health>();
+            if (hp != null) hp.TakeDamage(damage);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player") || !other.CompareTag("Shuriken"))
         {
+            print(other.name);
             Rigidbody rb = other.GetComponent<Rigidbody>();
-
-
             if (rb != null)
             {
                 if (other.gameObject.layer == 14)
@@ -31,17 +51,47 @@ public class PlayerMelee : MonoBehaviour
                     meleeImpact.PlayFeedbacks();
                 }
             }
+
+            Health hp = other.GetComponent<Health>();
+            if (hp != null)
+            {
+                print("fuck");
+                hp.TakeDamage(damage);
+            }
         }
 
-        Health hp = other.GetComponent<Health>();
-        if (hp != null)
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player") || !other.CompareTag("Shuriken"))
         {
-            if (other.CompareTag("Enemy"))
+            print(other.name);
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
             {
-                Debug.Log("Enemy Found");
+                if (other.gameObject.layer == 14)
+                {
+                    rb.velocity = Vector3.zero;
+                    other.GetComponent<EnemyBullet>().hurtEnemy = true;
+                    rb.AddForce(transform.forward * 25, ForceMode.Impulse);
+                    meleeImpact.PlayFeedbacks();
+                }
+                else
+                {
+                    rb.velocity = Vector3.zero;
+                    rb.AddForce(transform.forward * meleeKnockback.z + Vector3.up * meleeKnockback.y, ForceMode.Impulse);
+                    meleeImpact.PlayFeedbacks();
+                }
             }
 
-            hp.TakeDamage(damage);
+            Health hp = other.GetComponent<Health>();
+            if (hp != null)
+            {
+                print("fuck");
+                hp.TakeDamage(damage);
+            }
         }
     }
 }
